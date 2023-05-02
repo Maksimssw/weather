7aaabc5d8293f1665bc541dbb4bd30a667229c82
Maks <kiva_hill@mail.ru>
2023-05-02 16:19:27 +0300

    feat(layout): add component WeatherStatistic

--- src/components/Weather/WeatherBase/index.js
+++ src/components/Weather/WeatherBase/index.js
@@ -1,16 +1,40 @@
 import Cart from "../../UI/Cart";
 
-import styles from './index.module.css'
 import clouds from '../../../static/images/weather/clouds.webp'
+import mark from '../../../static/icons/mark.svg'
+
+import styles from './index.module.css'
+import WeatherStatistic from "./WeatherStatistic";
 
 const WeatherBase = () => {
   return (
     <Cart class={styles['weather-base']}>
-      <img
-        className={styles['weather-base__icon']}
-        src={clouds}
-        alt=""
-      />
+      <div>
+        <img
+          className={styles['weather-base__image']}
+          src={clouds}
+          alt=""
+        />
+
+        <div className={`${styles['weather-base__location']} grid-row`}>
+          <img
+            className='icon-20'
+            src={mark}
+            alt=""
+          />
+
+          <span>Rio do Sul, SC</span>
+        </div>
+
+        <div className={styles['weather-base__temp']}>
+          <span className={styles['weather-base__medium']}>18</span>
+          <span className={styles['weather-base__info']}>
+            22°
+            <i>16°</i>
+          </span>
+        </div>
+      </div>
+      <WeatherStatistic />
     </Cart>
   )
 }
--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,9 +1,9 @@
 import WeatherBase from "./WeatherBase";
 import WeatherInfo from "./WeatherInfo";
 import WeatherDays from "./WeatherDays";
+import WeatherHours from "./WeatherHours";
 
 import styles from './index.module.css'
-import WeatherHours from "./WeatherHours";
 
 const Weather = () => {
   return (
